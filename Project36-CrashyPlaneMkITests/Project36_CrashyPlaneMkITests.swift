99f056d44c3ff1a745c9af0ff71074b99db500cf
Noah Pope <npope@tutanota.com>
2026-02-03 14:16:56 -0600

    Initial Commit

---
+++ Project36-CrashyPlaneMkITests/Project36_CrashyPlaneMkITests.swift
@@ -0,0 +1,14 @@
+//  File: Project36_CrashyPlaneMkITests.swift
+//  Project: Project36-CrashyPlaneMkITests
+//  Created by: Noah Pope on 2/3/26.
+
+import Testing
+@testable import Project36_CrashyPlaneMkI
+
+struct Project36_CrashyPlaneMkITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
